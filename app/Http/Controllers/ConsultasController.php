b355a5175d88cb487481592fb34aabcb2f1f82b7
Juan David <juanprogamadorfullstack@gmail.com>
2026-05-10 12:35:47 -0500

    consultas y ruta

--- app/Http/Controllers/ConsultasController.php
+++ app/Http/Controllers/ConsultasController.php
@@ -4,12 +4,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\perfile;
+use App\Models\User;
+use App\Models\Feedback;
+use App\Models\CategoriaGlobal;
 
 class ConsultasController extends Controller
 {
     public function verUsuario(){
-        $perfil = Perfile::find(2);
+        $perfil = Perfile::find(4);
 
         return $perfil->user;
+    }
+
+    public function verPerfil(){
+        $user = User::find(3);
+
+        return $user->perfil;
+    }
+// CONSULTA PARA VER LOS FEEDBACKS DE LOS USUARIOS Y LOS NOMBRES DE LOS USUARIOS QUE LOS HAN ENVIADO
+    public function verFeedbacks(){
+        $feedback = Feedback::with('User')->get();
+        return $feedback;
+    }
+
+    public function verContenidoEducativo(){
+        $user = User::find(1);
+        $use = User::with('contenidosEducativos.categoriaGlobal')->get();
+        return $use;
     }
 }
